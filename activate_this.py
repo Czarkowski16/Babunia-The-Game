81a4c4d446b53ac969836cce1bb615093c12a49a
Czarek <139174737+Czarkowski16@users.noreply.github.com>
2024-06-15 11:32:23 +0200

    activate_this.py

--- activate_this.py
+++ activate_this.py
@@ -1,10 +1,10 @@
 import pygame
 import sys
 
-# Inicjalizacja Pygame
+
 pygame.init()
 
-# Ustawienia okna gry
+
 WIDTH, HEIGHT = 1220, 720
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Babunia")
@@ -12,13 +12,27 @@ pygame.display.set_caption("Babunia")
 # Kolory
 WHITE = (255, 255, 255)
 
-# Ładowanie sprite'a gracza
-player_image = pygame.image.load("PanPole.png")
-player_rect = player_image.get_rect()
-player_speed = 5
-player_image = pygame.transform.scale(player_image, (300, 300))
-# Ustawienie początkowej pozycji gracza
+standing_right = pygame.image.load("PanPole.png")
+step_right = pygame.image.load("2.png")
+standing_left = pygame.image.load("3.png")
+step_left = pygame.image.load("4.png")
+
+
+standing_right = pygame.transform.scale(standing_right, (200, 200))
+step_right = pygame.transform.scale(step_right, (200, 200))
+standing_left = pygame.transform.scale(standing_left, (200, 200))
+step_left = pygame.transform.scale(step_left, (200, 200))
+
+
+player_rect = standing_right.get_rect()
 player_rect.center = (WIDTH // 2, HEIGHT // 2)
+
+
+left = False
+right = False
+is_step = False
+facing_right = True
+player_speed = 8
 
 # Główna pętla gry
 running = True
@@ -27,25 +41,46 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # Ruch gracza
+   
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
         player_rect.x -= player_speed
-    if keys[pygame.K_RIGHT]:
+        left = True
+        right = False
+        is_step = not is_step  
+        facing_right = False
+    elif keys[pygame.K_RIGHT]:
         player_rect.x += player_speed
+        left = False
+        right = True
+        is_step = not is_step 
+        facing_right = True
+    else:
+        left = False
+        right = False
+
     if keys[pygame.K_UP]:
         player_rect.y -= player_speed
     if keys[pygame.K_DOWN]:
         player_rect.y += player_speed
 
-    # Aktualizacja okna gry
+    
     WIN.fill(WHITE)
-    WIN.blit(player_image, player_rect)
+
+    if left:
+        current_image = step_left if is_step else standing_left
+    elif right:
+        current_image = step_right if is_step else standing_right
+    else:
+        current_image = standing_right if facing_right else standing_left
+
+    WIN.blit(current_image, player_rect)
+
     pygame.display.update()
 
-    # Opóźnienie
-    pygame.time.delay(30)
+    
+    pygame.time.delay(100)
 
-# Zakończenie Pygame
+
 pygame.quit()
 sys.exit()
